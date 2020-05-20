8654ef6b7817fb908c2979bc50ec6b4020b3a227
FlorinEXable <iavflorin@gmail.com>
2020-05-20 13:50:13 +0300

    Adaugare Laborator 12

---
+++ Laborator 12/Web/Models/PhotoDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace Web.Models
+{
+        public partial class PhotoDTO
+        {
+            [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+            [DataMember]
+            public int Id { get; set; }
+            [DataMember]
+            public string Cale { get; set; }
+            [DataMember]
+            public Nullable<System.DateTime> DataCreare { get; set; }
+            [DataMember]
+            public string Eveniment { get; set; }
+            [DataMember]
+            public string Persoane { get; set; }
+            [DataMember]
+            public string Loc { get; set; }
+            [DataMember]
+            public string Peisaj { get; set; }
+            [DataMember]
+            public string Sters { get; set; }
+        }
+
+}
---
+++ Laborator 12/Web/Pages/Photos/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ServiceReferencePhoto;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages.Photos
+{
+    public class IndexModel : PageModel
+    {
+        InterfacePhotoClient ipc = new InterfacePhotoClient();
+        public List<PhotoDTO> Photos { get; set; }
+        public IndexModel()
+        {
+            Photos = new List<PhotoDTO>();
+        }
+        public async Task OnGetAsync()
+        {
+            var photos = await ipc.GetNamesAsync();
+            foreach(var item in photos)
+            {
+                PhotoDTO pd = new PhotoDTO();
+                pd.Cale = item;
+                pd.DataCreare = await ipc.GetDataCreariiAsync(item);
+                pd.Eveniment = await ipc.GetEvenimentAsync(item);
+                pd.Loc = await ipc.GetLocAsync(item);
+                pd.Persoane = await ipc.GetPersoaneAsync(item);
+                Photos.Add(pd);
+            }
+        }
+    }
+}
